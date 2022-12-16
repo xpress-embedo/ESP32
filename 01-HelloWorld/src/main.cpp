080a8409bad150bad3ac490b15e1e044558ba389
xpress-embedo <laboratory.embedded@gmail.com>
2022-12-16 20:05:07 +0100

    01-HelloWorld : DHT11 Sensor Working, MQTT In-Progress * DHT11 working * MQTT is also working, publish was working, subscription is not working debugging in progress * also facing some issues, sometime connection with router is not possible

--- 01-HelloWorld/src/main.cpp
+++ 01-HelloWorld/src/main.cpp
@@ -8,7 +8,8 @@
 #define DHTPIN                  (12)
 #define DHTTYPE                 (DHT11)
 
-#define DHT_REFRESH_TIME        (2000u)   // 2 seconds
+#define DHT_REFRESH_TIME        (20000u)   // 20 seconds
+#define MQTT_PUB_BUFF_SIZE      (20u)
 
 // Private Variables
 static uint32_t dht_refresh_timestamp = 0u;
@@ -19,16 +20,19 @@ DHT dht(DHTPIN, DHTTYPE);
 // Update these Information
 const char* ssid = "TestWiFi";                  // WiFi Name
 const char* password = "12345678";              // WiFi Password
-const char* mqtt_server = "m14.cloudmqtt.com";  // MQTT Server Name
-const int mqtt_port = 18410;                    // MQTT Server Port
-const char* user_name = "setsmjwc";             // MQTT Server Instance User Name
-const char* mqtt_pswd = "apDnKqHRgAjA";         // MQTT Server Instance Password
+const char* mqtt_server = "hairdresser.cloudmqtt.com";  // MQTT Server Name
+const int mqtt_port = 17259;                            // MQTT Server Port
+const char* user_name = "pyptiouq";                     // MQTT Server Instance User Name
+const char* mqtt_pswd = "aQp113ENJeO9";                 // MQTT Server Instance Password
 
 WiFiClient esp_client;
 PubSubClient client( esp_client );
+char mqtt_pub_msg[MQTT_PUB_BUFF_SIZE] = { 0 };
 
 // private function prototypes
 void setup_wifi( void );
+void mqtt_callback( char* topic, byte* payload, uint16_t length );
+void mqtt_reconnect( void );
 
 void setup() 
 {
@@ -41,12 +45,22 @@ void setup()
   
   // Setup ESP32 with WiFi
   setup_wifi();
+
+  client.setServer( mqtt_server, mqtt_port );
+  client.setCallback( mqtt_callback );
 }
 
 void loop()
 {
-    // DHT11 Refresh Timed Task
-  if( millis() - dht_refresh_timestamp >= DHT_REFRESH_TIME )
+  uint32_t now;
+  now = millis();
+  if( client.connected() == false )
+  {
+    mqtt_reconnect();
+  }
+
+  // DHT11 Refresh Timed Task
+  if( now - dht_refresh_timestamp >= DHT_REFRESH_TIME )
   {
     dht_refresh_timestamp = millis();
     // Reading temperature or humidity takes about 250 milliseconds!
@@ -67,6 +81,13 @@ void loop()
       Serial.print(F("%  Temperature: "));
       Serial.print(temperature);
       Serial.println(F("°C "));
+      // publishing data to mqtt server
+      snprintf( mqtt_pub_msg, MQTT_PUB_BUFF_SIZE, "%02d,%02d", \
+                (uint8_t)(temperature), (uint8_t)(humidity) );
+      // printing only for debugging purposes
+      Serial.print("Publish Message : ");
+      Serial.println( mqtt_pub_msg );
+      client.publish("home", mqtt_pub_msg );
     }
   }
 }
@@ -84,7 +105,7 @@ void setup_wifi( void )
 
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
+    delay(2000);
     Serial.print(".");
   }
 
@@ -92,4 +113,67 @@ void setup_wifi( void )
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+}
+
+void mqtt_callback( char* topic, byte* payload, uint16_t length )
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (uint16_t i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i], HEX);
+  }
+  Serial.println();
+
+  // Check which topic we have received and based on that take action
+  // led topic
+  if( strcmp(topic,"led") == 0 )
+  {
+    // Switch on the LED if an 1 was received as first character
+    if ((char)payload[0] == '1') 
+    {
+      // TODO: XS to connect an actual led
+      // digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on
+    }
+    else 
+    {
+      // TODO: XS to connect an actual led
+      // digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off
+    }
+  }
+}
+
+void mqtt_reconnect( void )
+{
+  // stay here/loop untill connected again
+  while( client.connected() == false )
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    // String clientId = "ESP8266Client-";
+    // clientId += String(random(0xffff), HEX);
+    String clientId = "EmbeddedLab";
+    Serial.print("Client Id:  ");
+    Serial.println(clientId);
+    // Attempt to connect
+    // if (client.connect(clientId.c_str())) 
+    if ( client.connect(clientId.c_str(), user_name, mqtt_pswd) )
+    {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      // client.publish("Broadcast", "Connected with MQTT Server");
+      // ... and resubscribe (Topic is "LED", to control the on board LED)
+      client.subscribe("led");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // TODO: XS this crude delay will block other tasks
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
