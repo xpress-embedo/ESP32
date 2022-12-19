bcbadc3d2d07671cc8264c0f4f96148e7996e8ae
Embedded Laboratory <laboratory.embedded@gmail.com>
2022-12-19 19:20:40 +0530

    02-DataLogger_InfluxDB working * sending RSSI value of WiFi to InfluxDB server is possible * this is working next step is to send temperature and humidity data to InfluxDB database

---
+++ 02-DataLogger_InfluxDB/src/main.cpp
@@ -0,0 +1,102 @@
+/**
+ * Basic Write Example code for InfluxDBClient library for Arduino
+ * Data can be immediately seen in a InfluxDB UI: wifi_status measurement
+ * Enter WiFi and InfluxDB parameters below
+ *
+ * Measures signal level of the actually connected WiFi network
+ * This example supports only InfluxDB running from unsecure (http://...)
+ * For secure (https://...) or Influx Cloud 2 use SecureWrite example
+ **/
+
+#include <Arduino.h>
+#include <WiFiMulti.h>
+#include <InfluxDbClient.h>
+#include <InfluxDbCloud.h>
+
+WiFiMulti wifiMulti;
+
+#define WIFI_SSID             "????????????"
+#define WIFI_PASSWORD         "????????????"
+#define INFLUXDB_URL          "????????????"
+#define INFLUXDB_TOKEN        "????????????"
+#define INFLUXDB_ORG          "????????????"
+#define INFLUXDB_BUCKET       "????????????"
+
+// Set timezone string according to https://www.gnu.org/software/libc/manual/html_node/TZ-Variable.html
+// Examples:
+//  Pacific Time: "PST8PDT"
+//  Eastern: "EST5EDT"
+//  Japanesse: "JST-9"
+//  Central Europe: "CET-1CEST,M3.5.0,M10.5.0/3"
+#define TZ_INFO "CET-1CEST,M3.5.0,M10.5.0/3"
+
+// InfluxDB client instance with preconfigured InfluxCloud Certificate
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
+
+#define DEVICE "ESP32"
+
+// Data point
+Point sensor("wifi_status");
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // Connect WiFi
+  Serial.println("Connecting to WiFi");
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+  while (wifiMulti.run() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+
+  // Add constant tags - only once
+  sensor.addTag("device", DEVICE);
+  sensor.addTag("SSID", WiFi.SSID());
+
+  // Accurate time is necessary for certificate validation and writing in batches
+  // For the fastest time sync find NTP servers in your area: https://www.pool.ntp.org/zone/
+  // Syncing progress and the time will be printed to Serial.
+  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+
+  // Check server connection
+  if (client.validateConnection()) 
+  {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  }
+  else
+  {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+}
+
+void loop()
+{
+  // Store measured value into point
+  sensor.clearFields();
+  // Report RSSI of currently connected network
+  sensor.addField("rssi", WiFi.RSSI());
+  // Print what are we exactly writing
+  Serial.print("Writing: ");
+  Serial.println(client.pointToLineProtocol(sensor));
+  // If no Wifi signal, try to reconnect it
+  if (wifiMulti.run() != WL_CONNECTED)
+  {
+    Serial.println("Wifi connection lost");
+  }
+  // Write point
+  if (!client.writePoint(sensor))
+  {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  //Wait 10s
+  Serial.println("Wait 10s");
+  delay(10000);
+}
