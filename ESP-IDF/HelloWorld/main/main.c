4981da991682f23dd519a3683e1914deb5c77f02
xpress-embedo <laboratory.embedded@gmail.com>
2022-12-28 13:38:59 +0100

    ESP-IDF : Hello World program updated to get data from DHT11 sensor (not working) * just changed the flash size from 2MB to 4MB * rest changes comes automatically
    * updated to get data from dht11, but this is not working, will investigate later
    * strangely the program from Arduino still works fine
    
    * esp-idf-lib components added in this file
    * this module is added as submodule, maybe in future I will use the fetch command or use module in a common folder.

--- ESP-IDF/HelloWorld/main/main.c
+++ ESP-IDF/HelloWorld/main/main.c
@@ -2,11 +2,43 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#include <dht.h>
+
+#define SENSOR_TYPE         (DHT_TYPE_DHT11)
+#define SENSOR_PIN          (GPIO_NUM_12)
+
+static void DHT_Measure( void *pvParameters );
+
 void app_main(void)
 {
+  // Create a task for reading DHT sensor data
+  xTaskCreate( DHT_Measure, "DHT Measure", configMINIMAL_STACK_SIZE*3u, NULL, 5, NULL);
   while (true)
   {
     printf("Hello from app_main!\n");
-    sleep(1);
+    vTaskDelay( pdMS_TO_TICKS(1000) );
+  }
+}
+
+static void DHT_Measure( void *pvParameters )
+{
+  float temperature, humidity;
+  // enable the pull-up on the data line if needed
+  gpio_set_pull_mode( SENSOR_PIN, GPIO_PULLUP_ONLY );
+  while(1)
+  {
+    if( dht_read_float_data( SENSOR_TYPE, SENSOR_PIN, &humidity, &temperature) == ESP_OK )
+    {
+      printf("Humidity: %.1f%% Temp: %.1fC\n", humidity, temperature);
+    }
+    else
+    {
+      printf("Could not read data from sensor\n");
+    }
+    // don't read too fast from the sensor
+    vTaskDelay( pdMS_TO_TICKS(2000) );
   }
 }
