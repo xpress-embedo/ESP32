3a6615e0b4f42b1f3b96ac15b0cc80ee5385678f
xpress-embedo <laboratory.embedded@gmail.com>
2022-12-29 18:21:32 +0100

    ESP-IDF : Simple Access Point Project --> updated to simple WebServer project * simple http server example is used * ledoff uri is created

--- ESP-IDF/WebserverAP/main/main.c
+++ ESP-IDF/WebserverAP/main/main.c
@@ -1,11 +1,4 @@
-/*  WiFi softAP Example
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -13,10 +6,14 @@
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
+#include "esp_system.h"
+#include "esp_netif.h"
+#include "esp_http_server.h"
 #include "nvs_flash.h"
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
+
 
 /* This Example use WiFi config. that you can set via project configuration menu
 
@@ -28,12 +25,35 @@
 #define EXAMPLE_ESP_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
 #define EXAMPLE_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
 
-static const char *TAG = "wifi softAP";
-
 // Private Function Declaration
 static void wifi_init_softap(void);
+
+static esp_err_t led_off_handler(httpd_req_t *req);
+static esp_err_t led_on_handler(httpd_req_t *req);
+static esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err);
+
+static httpd_handle_t start_webserver(void);
+static esp_err_t stop_webserver(httpd_handle_t server);
+
+// Handler Function
 static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                     int32_t event_id, void* event_data);
+static void connect_handler(void* arg, esp_event_base_t event_base,
+                            int32_t event_id, void* event_data);
+static void disconnect_handler(void* arg, esp_event_base_t event_base,
+                               int32_t event_id, void* event_data);
+
+// Private Constant Variables
+static const char *TAG = "WebServer";
+static const httpd_uri_t ledoff =
+{
+  .uri       = "/ledoff",
+  .method    = HTTP_GET,
+  .handler   = led_off_handler,
+  .user_ctx  = "Hello World!"
+};
+
+static httpd_handle_t server = NULL;
 
 
 void app_main(void)
@@ -87,6 +107,14 @@ static void wifi_init_softap( void )
                                                        &wifi_event_handler,\
                                                        NULL,
                                                        NULL) );
+  ESP_ERROR_CHECK(esp_event_handler_register( IP_EVENT, \
+                                              IP_EVENT_AP_STAIPASSIGNED, \
+                                              &connect_handler, \
+                                              &server) );
+  ESP_ERROR_CHECK(esp_event_handler_register( WIFI_EVENT, \
+                                              WIFI_EVENT_STA_DISCONNECTED, \
+                                              &disconnect_handler, \
+                                              &server) );
 
   wifi_config_t wifi_config =
   {
@@ -143,3 +171,96 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
     ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d", MAC2STR(event->mac), event->aid);
   }
 }
+
+static httpd_handle_t start_webserver(void)
+{
+  httpd_handle_t server = NULL;
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  config.lru_purge_enable = true;
+
+  // Start the httpd server
+  ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+  if (httpd_start(&server, &config) == ESP_OK)
+  {
+    // Set URI handlers
+    ESP_LOGI(TAG, "Registering URI handlers");
+    httpd_register_uri_handler(server, &ledoff);
+    return server;
+  }
+
+  ESP_LOGI(TAG, "Error starting server!");
+  return NULL;
+}
+
+static esp_err_t stop_webserver(httpd_handle_t server)
+{
+  // Stop the httpd server
+  return httpd_stop(server);
+}
+
+static void disconnect_handler(void* arg, esp_event_base_t event_base,
+                               int32_t event_id, void* event_data)
+{
+  httpd_handle_t* server = (httpd_handle_t*) arg;
+  if (*server)
+  {
+    ESP_LOGI(TAG, "Stopping webserver");
+    if (stop_webserver(*server) == ESP_OK)
+    {
+      *server = NULL;
+    }
+    else
+    {
+      ESP_LOGE(TAG, "Failed to stop http server");
+    }
+  }
+}
+
+static void connect_handler(void* arg, esp_event_base_t event_base,
+                            int32_t event_id, void* event_data)
+{
+  httpd_handle_t* server = (httpd_handle_t*) arg;
+  if (*server == NULL)
+  {
+    ESP_LOGI(TAG, "Starting webserver");
+    *server = start_webserver();
+  }
+}
+
+/* An HTTP GET handler */
+static esp_err_t led_off_handler( httpd_req_t *req )
+{
+  esp_err_t error;
+  ESP_LOGI( TAG, "LED Turned OFF" );
+  const char *response = (const char *)req->user_ctx;
+  error = httpd_resp_send(req, response, strlen(response) );
+  if( error != ESP_OK )
+  {
+    ESP_LOGI( TAG, "Error %d while sending Response", error );
+  }
+  else
+  {
+    ESP_LOGI( TAG, "Response Sent Successfully" );
+  }
+  return error;
+}
+
+/* This handler allows the custom error handling functionality to be
+ * tested from client side. For that, when a PUT request 0 is sent to
+ * URI /ctrl, the /hello and /echo URIs are unregistered and following
+ * custom error handler http_404_error_handler() is registered.
+ * Afterwards, when /hello or /echo is requested, this custom error
+ * handler is invoked which, after sending an error message to client,
+ * either closes the underlying socket (when requested URI is /echo)
+ * or keeps it open (when requested URI is /hello). This allows the
+ * client to infer if the custom error handler is functioning as expected
+ * by observing the socket state.
+ */
+static esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
+{
+  /* For any other URI send 404 and close socket */
+  httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "Some 404 error message");
+  return ESP_FAIL;
+}
+
+
