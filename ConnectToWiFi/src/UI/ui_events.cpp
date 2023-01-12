371edb193f6c1e56ac7972f370048ed1eb145564
xpress-embedo <laboratory.embedded@gmail.com>
2023-01-12 21:00:29 +0100

    ConnectToWiFi: connected to WiFi from the entered information, but still a lot of thing to do * the main problem is that I am not able to update the screen while re-scanning   forum post also created "https://forum.lvgl.io/t/force-display-update-with-blocking-functions/10679" * connecting to WiFi is done, but the major problem is entering password again and again is very tedious job   - need to find a way to store the information in flash and load at power-up
    * todo steps
      - progress bar should be there at start-up, else system looks hanged-up for sometime until wifi ssid scanning is completed
      - connecting to wifi, this is done, but the end result should be displaying something on the display
      - When Re-Scanning is started/initiated, display message box, to stop user from doing something else -> done but needs major update

--- ConnectToWiFi/src/UI/ui_events.cpp
+++ ConnectToWiFi/src/UI/ui_events.cpp
@@ -5,6 +5,13 @@
 
 #include "ui.h"
 #include "main.h"
+#include "WiFi.h"
+
+#define WIFI_SSID_BUFFER_SIZE                     (20u)
+#define WIFI_PSWD_BUFFER_SIZE                     (20u)
+
+char wifi_ssid[WIFI_SSID_BUFFER_SIZE] = { 0 };
+char wifi_pswd[WIFI_PSWD_BUFFER_SIZE] = { 0 };
 
 void ShowKeyBoard(lv_event_t * e)
 {
@@ -21,10 +28,35 @@ void HideKeyBoard(lv_event_t * e)
 void ConnectToRouter(lv_event_t * e)
 {
   LV_LOG_USER("Ready to be Connected to the Router");
+  // first step is to get the ssid name and password
+  lv_dropdown_get_selected_str(ui_DropDownSSID, wifi_ssid, WIFI_SSID_BUFFER_SIZE);
+  LV_LOG_USER("WIFI SSID: %s", wifi_ssid );
+  LV_LOG_USER("WIFI PSWD: %s", lv_textarea_get_text(ui_TextAreaPassword) );
+
+  // Try to Connect with the Router
+  WiFi.begin( wifi_ssid, lv_textarea_get_text(ui_TextAreaPassword) );
+
+  while( WiFi.status() != WL_CONNECTED )
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void ReScanWiFiSSID(lv_event_t * e)
 {
+  static lv_obj_t *msg_box;
+  // msgbox not working, reason is known, will work on this later
+  // msg_box = lv_msgbox_create( NULL, "Re-Scanning", \
+  //                             "Please Wait, getting WiFi SSID", \
+  //                             NULL, false );
+  // lv_obj_center(msg_box);
   WiFi_Init();
   WiFi_ScanSSID();
+  // lv_msgbox_close( msg_box );
 }
+
