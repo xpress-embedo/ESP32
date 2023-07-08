2f8443b5c41a3fedf2b33c474bd9883af9365cd0
xpress-embedo <laboratory.embedded@gmail.com>
2023-07-08 15:18:57 +0200

    OpenWeatherMap Project * Working on GUI Part   - GUI folder contains the Square Line Studio Project   - and then the generated important files are added into the main folder * at the moment only first screen by default screen is displayed

--- ESP-IDF/OpenWeatherMap/main/display_mng.c
+++ ESP-IDF/OpenWeatherMap/main/display_mng.c
@@ -6,20 +6,16 @@
  */
 
 #include "display_mng.h"
-#include "lvgl.h"
 #include "bsp/esp-bsp.h"
+#include "ui.h"
 
 // Public Function Definitions
 void display_init(void)
 {
   // Start LVGL and LCD Driver
   bsp_display_start();
-
   bsp_display_lock(0);
-  /*Create a window*/
-  lv_obj_t *win = lv_win_create(lv_scr_act(), 40);
-  assert(win);
-  lv_win_add_title(win, "ESP32-S2-Kaluga-Kit:\nBoard Support Package example");
+  ui_init();
   bsp_display_unlock();
 }
 
