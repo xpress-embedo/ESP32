89261e3a0045a013650edd2bd269f4e77b98e311
xpress-embedo <laboratory.embedded@gmail.com>
2023-07-09 10:05:08 +0200

    Minor Fixes

--- ESP-IDF/OpenWeatherMap/main/main.c
+++ ESP-IDF/OpenWeatherMap/main/main.c
@@ -125,10 +125,8 @@ esp_err_t connect_wifi(void)
   {
     .sta =
     {
-      // .ssid = "ssid name",
-      // .password = "password",
-      .ssid = "gigacube-D733",
-      .password = "5yR8m2B6Y4egrJQ5",
+      .ssid = "ssid name",
+      .password = "password",
       .threshold.authmode = WIFI_AUTH_WPA2_PSK,
       .pmf_cfg =
       {
