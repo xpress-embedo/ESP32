b2c0550a9548d95186290fd053f4a9ad64b10f02
xpress-embedo <laboratory.embedded@gmail.com>
2023-07-25 23:08:23 +0200

    Added the program for MPU6050 Sensor Interfacing with ESP32, documentation to be updated later. tested this with Qt application, it is working fine

---
+++ AccelerometerVisualization/AccelerometerVisualization.ino
@@ -0,0 +1,49 @@
+/* Get tilt angles on X and Y, and rotation angle on Z
+ * Angles are given in degrees
+ * 
+ * License: MIT
+ */
+
+#include "Wire.h"
+#include <MPU6050_light.h>
+
+MPU6050 mpu(Wire);
+unsigned long timer = 0;
+
+void setup() 
+{
+  Serial.begin(115200);
+  Wire.begin();
+  
+  byte status = mpu.begin();
+//  Serial.print(F("MPU6050 status: "));
+//  Serial.println(status);
+  while(status!=0){ } // stop everything if could not connect to MPU6050
+  
+//  Serial.println(F("Calculating offsets, do not move MPU6050"));
+  delay(1000);
+  // mpu.upsideDownMounting = true; // uncomment this line if the MPU6050 is mounted upside-down
+  mpu.calcOffsets(); // gyro and accelero
+//  Serial.println("Done!\n");
+}
+
+void loop() 
+{
+  mpu.update();
+  if( (millis()-timer) > 100 )
+  { 
+    /*
+  	Serial.print("X : ");
+  	Serial.print(mpu.getAngleX());
+  	Serial.print("\tY : ");
+  	Serial.print(mpu.getAngleY());
+  	Serial.print("\tZ : ");
+  	Serial.println(mpu.getAngleZ());
+    */
+    // Printing Data as per our Qt Application format
+    Serial.print(mpu.getAngleX());
+    Serial.print(",");
+    Serial.println(mpu.getAngleY());
+  	timer = millis();
+  }
+}
