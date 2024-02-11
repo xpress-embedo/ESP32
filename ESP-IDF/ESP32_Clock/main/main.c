a5af5306979a6aa9a58be4bec0635fdd17fd69fc
xpress-embedo <laboratory.embedded@gmail.com>
2024-02-11 11:53:35 +0100

    ESP32 Clock * Integrated the SNTP, and it's working fine, I am able to get the time data from the ntp servers. * next step is to create a UI in SquareLineStudio

---
+++ ESP-IDF/ESP32_Clock/main/main.c
@@ -0,0 +1,190 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/task.h"
+
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_wifi.h"
+
+#include "sntp_time_sync.h"
+
+// Private Macros
+//#define APP_WIFI_SSID                       "Enter WIFI SSID"
+//#define APP_WIFI_PSWD                       "Enter WiFI Password"
+#define APP_WIFI_SSID                       "gigacube-D733"
+#define APP_WIFI_PSWD                       "5yR8m2B6Y4egrJQ5"
+#define WIFI_MAX_RETRY                      (5)
+#define WIFI_CONNECT_DELAY                  (500)     // Initial delay in milliseconds
+#define WIFI_MAX_DELAY                      (60000)   // Maximum delay in milliseconds
+// The following are the bits/flags for event group
+#define WIFI_CONNECTED_BIT                  BIT0      // connected to the access point with an IP
+#define WIFI_FAIL_BIT                       BIT1      // failed to connect after the max. amount of retries
+
+// Private Variables
+static const char *TAG = "APP";
+/* WiFi Connection Related Variables */
+static EventGroupHandle_t wifi_event_group;           // FreeRTOS event group to signal when we are connected
+static uint8_t wifi_connect_retry = 0;
+static bool wifi_connect_status = false;
+
+// Private Function Declarations
+static void app_connect_wifi( void );
+static void wifi_event_handler( void *arg, esp_event_base_t event_base, int32_t event_id, void * event_data );
+
+void app_main(void)
+{
+  // Disable default gpio logging messages
+  esp_log_level_set("gpio", ESP_LOG_NONE);
+  // disable default wifi logging messages
+  esp_log_level_set("wifi", ESP_LOG_NONE);
+
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+  {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+
+  // connect with WiFi (it will take some time)
+  app_connect_wifi();
+
+  if( wifi_connect_status )
+  {
+    sntp_time_sync_start();
+  }
+
+  while (true)
+  {
+    sleep(10);
+    sntp_time_sync_get_time();
+  }
+}
+
+// Private Function Definitions
+/**
+ * @brief Connect with the WiFi Router
+ * @note  in future this function can be moved to a commom place.
+ * @param  none
+ */
+static void app_connect_wifi( void )
+{
+  wifi_event_group = xEventGroupCreate();
+
+  ESP_ERROR_CHECK( esp_netif_init() );
+
+  ESP_ERROR_CHECK( esp_event_loop_create_default() );
+  esp_netif_create_default_wifi_sta();
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+  ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+
+  esp_event_handler_instance_t instance_any_id;
+  esp_event_handler_instance_t instance_got_ip;
+  ESP_ERROR_CHECK( esp_event_handler_instance_register( WIFI_EVENT,           \
+                                                        ESP_EVENT_ANY_ID,     \
+                                                        &wifi_event_handler,  \
+                                                        NULL,                 \
+                                                        &instance_any_id) );
+  ESP_ERROR_CHECK( esp_event_handler_instance_register( IP_EVENT,             \
+                                                        IP_EVENT_STA_GOT_IP,  \
+                                                        &wifi_event_handler,  \
+                                                        NULL,                 \
+                                                        &instance_got_ip) );
+
+  wifi_config_t wifi_config =
+  {
+    .sta =
+    {
+      .ssid = APP_WIFI_SSID,
+      .password = APP_WIFI_PSWD,
+      .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+    },
+  };
+
+  ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+  ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+  ESP_ERROR_CHECK( esp_wifi_start() );
+
+  ESP_LOGI(TAG, "WiFi Initialized in Station Mode Finished.");
+
+  /*
+   * Wait until either the connection is established (WIFI_CONNECTED_BIT) or
+   * connection failed for the maximum number of re-tries (WIFI_FAIL_BIT).
+   * The bits are set by event_handler() (see above) */
+  EventBits_t bits = xEventGroupWaitBits( wifi_event_group,                   \
+                                          WIFI_CONNECTED_BIT | WIFI_FAIL_BIT, \
+                                          pdFALSE,                            \
+                                          pdFALSE,                            \
+                                          portMAX_DELAY);
+
+  /* xEventGroupWaitBits() returns the bits before the call returned, hence we
+   * can test which event actually happened. */
+  if( bits & WIFI_CONNECTED_BIT )
+  {
+    ESP_LOGI(TAG, "Connected to Access Point %s", APP_WIFI_SSID );
+  }
+  else if( bits & WIFI_FAIL_BIT )
+  {
+    ESP_LOGE(TAG, "Failed to Connect to Access Point %s", APP_WIFI_SSID );
+  }
+  else
+  {
+    ESP_LOGE(TAG, "Unexpected Event" );
+  }
+  vEventGroupDelete(wifi_event_group);
+}
+
+/**
+ * @brief WiFi Event Handler Function
+ * @param arg 
+ * @param event_base Event Base whether WIFI Event or IP Event
+ * @param event_id   Event ID 
+ * @param event_data Data with Event
+ */
+static void wifi_event_handler( void *arg, esp_event_base_t event_base, int32_t event_id, void * event_data )
+{
+  if( WIFI_EVENT == event_base )
+  {
+    if( WIFI_EVENT_STA_START == event_id )
+    {
+      esp_wifi_connect();
+    }
+    else if( WIFI_EVENT_STA_DISCONNECTED == event_id )
+    {
+      if( wifi_connect_retry < WIFI_MAX_RETRY )
+      {
+        uint32_t delay = (1 << wifi_connect_retry) * WIFI_CONNECT_DELAY;
+        delay = (delay > WIFI_MAX_DELAY) ? WIFI_MAX_DELAY : delay;
+        // waiting for some time before retrying again
+        vTaskDelay(delay / portTICK_PERIOD_MS);
+        esp_wifi_connect();
+        wifi_connect_retry++;
+        ESP_LOGI(TAG, "Retry Wi-Fi connection (%d/%d)...", wifi_connect_retry, WIFI_MAX_RETRY);
+      }
+      else
+      {
+        wifi_connect_status = false;
+        xEventGroupSetBits(wifi_event_group, WIFI_FAIL_BIT);
+        ESP_LOGE(TAG, "Failed to connect to Access Point.");
+      }
+    }
+  } // if( WIFI_EVENT = event_base )
+  else if( IP_EVENT == event_base )
+  {
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+    ESP_LOGI(TAG, "Got IP:" IPSTR, IP2STR(&event->ip_info.ip));
+    wifi_connect_retry = 0;
+    wifi_connect_status = true;
+    xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
+  }
+}
+
---
+++ ESP-IDF/ESP32_Clock/main/sntp_time_sync.h
@@ -0,0 +1,15 @@
+/*
+ * sntp_time_sync.h
+ *
+ *  Created on: Feb 11, 2024
+ *      Author: xpress_embedo
+ */
+
+#ifndef MAIN_SNTP_TIME_SYNC_H_
+#define MAIN_SNTP_TIME_SYNC_H_
+
+// Public Function Declaration
+void sntp_time_sync_start( void );
+char * sntp_time_sync_get_time( void );
+
+#endif /* MAIN_SNTP_TIME_SYNC_H_ */
