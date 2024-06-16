f85dda6e1c2fb39e0a8de20081102cd951d73fe4
xpress-embedo <laboratory.embedded@gmail.com>
2024-06-16 14:34:50 +0530

    Weather Station Web Server: In-Progress * porting ESP32 Weather Station Web Server application to ESP32S3 Sunton board

---
+++ ESP-IDF/S3_WeatherStationServer/main/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "driver/gpio.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+
+#include "main.h"
+#include "dht11.h"
+
+// macros
+#define DHT11_PIN                           (GPIO_NUM_17)
+#define MAIN_TASK_PERIOD                    (6000)
+
+// Private Variables
+static const char *TAG = "APP";
+/* Sensor Related Variables */
+static sensor_data_t sensor_data = { .sensor_idx = 0 };
+
+void app_main(void)
+{
+  // Disable default gpio logging messages
+  esp_log_level_set("gpio", ESP_LOG_NONE);
+
+  // initialize dht sensor library
+  dht11_init(DHT11_PIN, true);
+  while(1)
+  {
+    // Get DHT11 Temperature and Humidity Values
+    if( dht11_read().status == DHT11_OK )
+    {
+      uint8_t temp = (uint8_t)dht11_read().humidity;
+      // humidity can't be greater than 100%, that means invalid data
+      if( temp < 100 )
+      {
+        if( sensor_data.sensor_idx < SENSOR_BUFF_SIZE )
+        {
+          sensor_data.humidity[sensor_data.sensor_idx] = temp;
+          sensor_data.humidity_current = temp;
+          temp = (uint8_t)dht11_read().temperature;
+          sensor_data.temperature[sensor_data.sensor_idx] = temp;
+          sensor_data.temperature_current = temp;
+          ESP_LOGI(TAG, "Temperature: %d", sensor_data.temperature_current);
+          ESP_LOGI(TAG, "Humidity: %d", sensor_data.humidity_current);
+          sensor_data.sensor_idx++;
+          // reset the index
+          if( sensor_data.sensor_idx >= SENSOR_BUFF_SIZE )
+          {
+            sensor_data.sensor_idx = 0;
+          }
+        }
+      }
+      else
+      {
+        ESP_LOGE(TAG, "In-correct data received from DHT11 -> %u", temp);
+      }
+    }
+    else
+    {
+      ESP_LOGE(TAG, "Unable to Read DHT11 Status");
+    }
+    // Wait before next measurement
+    vTaskDelay(MAIN_TASK_PERIOD / portTICK_PERIOD_MS);
+  }
+}
