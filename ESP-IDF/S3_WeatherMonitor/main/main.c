d59dd3109798b453eafa196ea1cbcc52b9a45779
xpress-embedo <laboratory.embedded@gmail.com>
2025-08-26 21:02:50 +0530

    S3_WeatherMonitor: DHT11 Sensor working * next steps including WiFi and InfluxDB code

--- ESP-IDF/S3_WeatherMonitor/main/main.c
+++ ESP-IDF/S3_WeatherMonitor/main/main.c
@@ -1,21 +1,51 @@
+
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+
 #include "main.h"
 #include "gui_mng.h"
+#include "dht11.h"
 
 // Private Macros
 #define MAIN_TASK_PERIOD                (5000)
+#define DHT11_GPIO_NUM              		(GPIO_NUM_17)
 
 // Private Variables
 static const char *TAG = "MAIN";
+static uint8_t humidity = 0u;
+static uint8_t temperature = 0u;
 
 void app_main(void)
 {
+  dht11_sensor_t dht11_value;
+
+  // Initialize NVS
+  esp_err_t ret = nvs_flash_init();
+  if ( (ret == ESP_ERR_NVS_NO_FREE_PAGES) || (ret == ESP_ERR_NVS_NEW_VERSION_FOUND) )
+  {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+  
 	ESP_LOGI( TAG, "Starting Program");
+	
+	// initialize the dht11 module
+  dht11_init(DHT11_GPIO_NUM);
 	
 	gui_start();
 	
 	while( 1 )
 	{
-		ESP_LOGI( TAG, "Working");
-		vTaskDelay(MAIN_TASK_PERIOD / portTICK_PERIOD_MS );
+		// read the data from dht11 sensor
+    dht11_value = dht11_read();
+    // If reading is valid then only print
+    if( dht11_value.status == DHT11_OK )
+    {
+      temperature = (uint8_t)dht11_value.temperature;
+      humidity = (uint8_t)dht11_value.humidity;
+      ESP_LOGI(TAG, "Temperature:%d, Humidity:%d", temperature, humidity);
+    }
+    vTaskDelay(MAIN_TASK_PERIOD / portTICK_PERIOD_MS);
 	}
 }
