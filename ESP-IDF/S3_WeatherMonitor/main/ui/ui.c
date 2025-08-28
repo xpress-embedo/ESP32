caea482180e783223caf624d0ca409727893af2c
xpress-embedo <laboratory.embedded@gmail.com>
2025-08-28 14:04:08 +0530

    S3_WeatherMonitor: Temperature and Humidity is working as expected * next step is to make arc working * then Grafana

--- ESP-IDF/S3_WeatherMonitor/main/ui/ui.c
+++ ESP-IDF/S3_WeatherMonitor/main/ui/ui.c
@@ -33,14 +33,14 @@ void ui_init(void)
     lv_theme_t * theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED),
                                                false, LV_FONT_DEFAULT);
     lv_disp_set_theme(dispp, theme);
-    ui_SensorScreen_screen_init();
     ui_MainScreen_screen_init();
+    ui_SensorScreen_screen_init();
     ui____initial_actions0 = lv_obj_create(NULL);
-    lv_disp_load_scr(ui_SensorScreen);
+    lv_disp_load_scr(ui_MainScreen);
 }
 
 void ui_destroy(void)
 {
-    ui_SensorScreen_screen_destroy();
     ui_MainScreen_screen_destroy();
+    ui_SensorScreen_screen_destroy();
 }
--- ESP-IDF/S3_WeatherMonitor/main/ui/ui.h
+++ ESP-IDF/S3_WeatherMonitor/main/ui/ui.h
@@ -18,8 +18,8 @@ extern "C" {
 
 ///////////////////// SCREENS ////////////////////
 
-#include "screens/ui_SensorScreen.h"
 #include "screens/ui_MainScreen.h"
+#include "screens/ui_SensorScreen.h"
 
 ///////////////////// VARIABLES ////////////////////
 
